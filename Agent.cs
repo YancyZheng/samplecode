9480f6503d85a9672251f7f9ac200ce5c90be541
Yan <yancyzheng223@gmail.com>
2016-05-31 09:58:02 -0400

    main project

---
+++ Agent.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegatepart
+{
+    delegate void NewInfoEventHandler(NewInfoEventArgs e);
+    class Agent
+    {
+        public event NewInfoEventHandler newInfoEvent;
+
+        public void startWork(string newInfo)
+        {
+            NewInfoEventArgs e = new NewInfoEventArgs();
+            e._newInfo = newInfo;
+
+            onNewInfo(e);
+        }
+        protected virtual void onNewInfo(NewInfoEventArgs e)
+        {
+            if (newInfoEvent != null)
+            {
+                newInfoEvent(e);
+            }
+        }
+    }
+    class NewInfoEventArgs : EventArgs
+    {
+        public string _newInfo;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegatepart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Agent agent = new Agent();
+            agent.newInfoEvent += new NewInfoEventHandler(MikeFamily);
+            agent.newInfoEvent += new NewInfoEventHandler(TomFamily);
+            agent.newInfoEvent += new NewInfoEventHandler(YancyFamily);
+
+            agent.startWork("New information!!");
+        }
+
+        public static void MikeFamily(NewInfoEventArgs e)
+        {
+            Console.WriteLine("Mike got " + e._newInfo);
+        }
+
+        public static void TomFamily(NewInfoEventArgs e)
+        {
+            Console.WriteLine("Tom got " + e._newInfo);
+        }
+
+        public static void YancyFamily(NewInfoEventArgs e)
+        {
+            Console.WriteLine("Yancy got " + e._newInfo);
+        }
+    }
+}
